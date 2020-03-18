d95d51dd500050296c1d34f9cb4507b970ba37d1
Evstifeev Roman <someuniquename@gmail.com>
2020-03-18 20:44:59 +0300

    initial

---
+++ doublecode.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import json
+from collections import namedtuple, defaultdict
+from csv import reader as csvreader
+
+import responder
+import httpx
+import environ
+
+SRCDIR = environ.Path(__file__) - 1  # ./
+
+env = environ.Env()
+env.read_env(SRCDIR('env-local'))
+
+#client = httpx.Client()
+
+if 'CSV_PAIRS_URL' in env:
+    print(env('CSV_PAIRS_URL'))
+    response = httpx.get(env('CSV_PAIRS_URL').strip(), timeout=25)
+    codes = dict(csvreader(response.text.splitlines(), delimiter=','))
+    #codes = dict(csvreader('a,b\nc,d'.splitlines(), delimiter=','))
+        
+app = responder.API()
+
+@app.route("/")
+async def home(req, resp):
+    resp.content = app.template('home.html', env=env)
+
+@app.route("/getvalue/{key}")
+async def get(req, resp, *, key: str):
+    resp.text = codes.get(key)
+            
+if __name__ == '__main__':
+    app.run(debug=env.bool('WEB_DEBUG', default=False), port=env('PORT', default=8000))
